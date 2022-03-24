ddb839dfd930a85c3bbf674c9450375290d7d60e
Tagadda <36127788+Tagadda@users.noreply.github.com>
2022-03-24 09:38:44 +0000

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,6 +9,8 @@ pkg_dependencies="postgresql postgresql-contrib libpq-dev"
 
 NODEJS_VERSION="14"
 
+main_domain=$(cat /etc/yunohost/current_host)
+
 #=================================================
 # PERSONAL HELPERS
 #=================================================
